4e04b7f29c65613cc3cddc211319308221375318
KwekuDar <kwekudar@outlook.com>
2022-02-11 01:10:21 -0500

    added lyrics

--- pomify/src/Dashboard.js
+++ pomify/src/Dashboard.js
@@ -25,7 +25,7 @@ export default function Dashboard({code}) {
 
     useEffect(() => {
         if(!playingTrack) return
-        axios.get('http://localhost:300/lyrics', {
+        axios.get('http://localhost:3001/lyrics', {
             params: {
                 track:playingTrack.title,
                 artist: playingTrack.artist
